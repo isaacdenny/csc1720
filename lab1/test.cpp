bbcbc66be3f3b99cd09641e5753d06240a82d21d
Isaac Denny <isaac_denny@icloud.com>
2023-08-24 08:23:04 -0400

    file

--- lab1/test.cpp
+++ lab1/test.cpp
@@ -7,7 +7,18 @@ Programmer: Isaac Denny
 using namespace std;
 
 int main() {
-  // get input
-  // calculate
-  // output
+  int quantity = 0;
+  double price = 1.5;
+  double total = 0.0;
+
+  cout << "How many donuts would you like (Enter an int): ";
+  cin >> quantity;
+
+  total = quantity * price;
+
+  cout << "Your total is: $ ";
+  cout << total;
+  cout << endl;
+
+  return 0;
 }
