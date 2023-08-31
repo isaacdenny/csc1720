b913f2609ec92634630587265475212cea60a297
Isaac Denny <isaac_denny@icloud.com>
2023-08-31 08:09:03 -0400

    partb

--- lab2/parta.cpp
+++ lab2/parta.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+
 using namespace std;
 
 int main() {
 	int num1;
 	int num2;
-	cout << "Enter the first int: ";
-	cin >> num1;
-	cout << "Enter the second int: ";
-	cin >> num2;
-	cout << "The sum is: " << num1 + num2;
+	ifstream inFile;
+	ofstream outFile;
+
+	inFile.open("inputFile.txt");
+	outFile.open("outputFile.txt");
+
+	while (!inFile.eof()) {
+		inFile >> num1;
+		inFile >> num2;
+		int sum = num1 + num2;
+		cout << num1 << " + " << num2 << " = " << sum << endl;
+	}
+
+	inFile.close();
+	outFile.close();
 	return 0;
 }
---
+++ lab2/partb.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int num1;
+	int num2;
+	cout << "Enter the first int: ";
+	cin >> num1;
+	cout << "Enter the second int: ";
+	cin >> num2;
+	cout << "The sum is: " << num1 + num2;
+	return 0;
+}
