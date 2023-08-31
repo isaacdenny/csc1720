8b62d0ef158671410ca09bb246393ff35288aef0
Isaac Denny <isaac_denny@icloud.com>
2023-08-31 08:32:10 -0400

    format

--- lab2/partc.cpp
+++ lab2/partc.cpp
@@ -18,8 +18,8 @@ int main() {
 	inFile.open(inputFileName);
 	outFile.open("PayReport.txt");
 
-	outFile << left << setw(12) << "Last"  << setw(12) << "First" << setw(12) << right << "Old Pay" << setw(12) << "New Pay" << endl;
-	outFile << left << setw(12) << "-----------" << setw(12) << "---------" << setw(12) << right << "----------" << setw(12) << "----------" << endl;
+	outFile << left << setw(12) << "Last"  << setw(11) << "First" << setw(12) << right << "Old Pay" << setw(12) << "New Pay" << endl;
+	outFile << left << setw(12) << "-----------" << setw(11) << "---------" << setw(12) << right << "----------" << setw(12) << "----------" << endl;
 	outFile << fixed << showpoint << setprecision(2);
 
 	while (!inFile.eof()) {
@@ -28,7 +28,7 @@ int main() {
 		inFile >> oldPay;
 		
 		double newPay = oldPay * 1.08f;
-		outFile << left << setw(12) << lastName + "," << setw(12) << firstName << setw(12) << right << oldPay << setw(12) << newPay << endl;
+		outFile << left << setw(12) << lastName + "," << setw(11) << firstName << setw(12) << right << oldPay << setw(12) << newPay << endl;
 	}
 
 	inFile.close();
