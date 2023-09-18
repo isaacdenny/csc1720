7e7febe6c875be07645b8aafe8450103cbf99d5e
Isaac Denny <isaac_denny@icloud.com>
2023-09-18 08:25:08 -0400

    milli

--- lab4/helpers.cpp
+++ lab4/helpers.cpp
@@ -74,7 +74,7 @@ void lazyPrint(string toPrint, int time, bool endline) {
 	while (toPrint.length() > 0) {
 		cout << toPrint[0];
 		toPrint.erase(0, 1);
-		sleep(time);
+		sleep(time / 100);
 	}
 	if (endline) {
 		cout << endl;
